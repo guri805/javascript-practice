45c07cf71d4d5a452bb81c6a1fe9d6802c09b0ac
Gursangam singh <gursangamsingh2@gmail.com>
2024-09-01 19:32:22 +0530

    add more info

--- self/v2_datatype_conversion.js
+++ self/v2_datatype_conversion.js
@@ -26,3 +26,18 @@ let valueInNumber4=String(score4);
 console.log(typeof valueInNumber4);//number
 console.log(valueInNumber4);//0
 
+/* Return type of variables in JavaScript
+1) Primitive Datatypes
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+
+2) Non-primitive Datatypes
+       Arrays  =>  object
+       Function  =>  function
+       Object  =>  object
+*/       
