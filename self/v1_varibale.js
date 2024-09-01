c51cd7bda2e3faf6e7282a4629b15cacdcc62dfe
Gursangam singh <gursangamsingh2@gmail.com>
2024-09-01 14:40:55 +0530

    variable lecture 1

---
+++ self/v1_varibale.js
@@ -0,0 +1,14 @@
+const accountId=1334;
+let accountEmail="guri11@gmail.com";
+var accountPassword="239422fd";
+accountcity="jaipur";
+let accountstate;
+
+// change the values //the value of const will not be changed
+
+accountEmail="harsh@gmail.com";
+accountPassword="2gaae23";
+accountcity="patiala";
+accountstate;
+
+console.table([accountId,accountEmail,accountPassword,accountcity,accountstate]);
