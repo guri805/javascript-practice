6c846b3030db90e1a7598cf01dc490a59953597e
Gursangam singh <gursangamsingh2@gmail.com>
2024-09-07 19:17:09 +0530

    Arrays and objects

---
+++ self/arrays/a2_basic.js
@@ -0,0 +1,46 @@
+const marvelHeros = [`thor`,`ironman`,`spiderman`];
+const dcHeros = [`superman`,`flash`,`batman`];
+const indHeros = [`shaktiman`];
+
+// METHOD 1 push the dcHeros to marvelHeros 
+// marvelHeros.push(dcHeros);
+// console.log(marvelHeros); //[ 'thor', 'ironman', 'spiderman', [ 'superman', 'flash', 'batman' ] ]
+// during push the all the dcHeros array will push to the 3 place of array 
+// console.log(marvelHeros[3][1]);
+
+// METHOD 2 concat function to merge a array 
+// const allHeros = marvelHeros.concat(dcHeros);
+// console.log(allHeros); //[ 'thor', 'ironman', 'spiderman', 'superman', 'flash', 'batman' ]
+// it will merge two array in a new single array
+
+// METHOD 3 by using spread operator
+// in which we can merge more than two arrays
+const allHeros = [...marvelHeros,...dcHeros,...indHeros]; 
+console.log(allHeros); // ['thor','ironman','spiderman','superman','flash','batman','shaktiman']
+
+// flat method using when we have multiple depth array
+const depthArray = [1,2,3,[4,5,6],7,[6,7,[4,5]]];
+const real_depthArray = depthArray.flat(Infinity)
+console.log(real_depthArray); // [1, 2, 3, 4, 5,6, 7, 6, 7, 4,5]
+
+// when the data is comming from the another side like(node data) we can check the data is in array type or not and convert them into array type
+console.log(Array.isArray(`gursangam`)); //check the type  //false
+console.log(Array.from(`gursangam`));   //convert to array //['g', 'u', 'r','s', 'a', 'n','g', 'a', 'm']
+console.log(Array.from({fullName : `gursangam`})); // it will give empty string we want to specity we can convert key or value //[]
+
+let score1 = 100 ;
+let score2 = 200 ;
+let score3 = 300 ;
+console.log(Array.of(score1,score2,score3)); //[ 100, 200, 300 ]
+ 
+
+
+
+
+
+
+
+
+
+
+
---
+++ self/objects/o1_basics.js
@@ -0,0 +1,45 @@
+// single ton
+// object.
+
+// object iteration
+
+const mySym = Symbol("key1");
+
+const jsUser = {
+  name : "gursangam",
+  "full name" : "gursangam singh",
+  age : 18 ,
+  location : "mohali",
+  email : "guri@gmail.com" ,
+  [mySym]: "myKey1" ,
+  isLoggedIn : false ,
+  lastLogginDays :["monday" , "Friday"]
+}
+
+// console.log(jsUser.email); // guri@gmail.com it gives output but the value is in string it will give error ("email")
+// // to avoid this we use 
+// console.log(jsUser[`email`]); //guri@gmail.com
+// // console.log(jsUser.full name); error
+// console.log(jsUser["full name"]);
+// console.log(jsUser[mySym]);
+// console.log(jsUser);
+ 
+// change the value
+// jsUser["email"] = "guri@pro.com";
+// console.log(jsUser["email"]);
+
+// Object.freeze(jsUser); in which the value cannot be updated
+
+jsUser.greeting = function () {
+  console.log("welcome js user");
+}
+console.log(jsUser.greeting());
+
+jsUser.greetingTwo = function () {
+  console.log(`welcome js user, ${this["full name"]}`);
+}
+console.log(jsUser.greetingTwo());
+
+
+
+
