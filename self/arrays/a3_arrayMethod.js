70b88e4e0908298d9991d65e6826f2ba8b9efb71
Gursangam singh <gursangamsingh2@gmail.com>
2024-09-08 12:24:36 +0530

    array methods

---
+++ self/arrays/a3_arrayMethod.js
@@ -0,0 +1,21 @@
+const arr = [1 , 2 , 3 , 4];
+
+// array methods 
+
+// console.log(arr.length);        // 4
+// arr.push(5);                   //[ 1, 2, 3, 4, 5 ]
+// arr.pop(5);                    //[ 1, 2, 3, 4]
+// arr.unshift(0);                //[ 0, 1, 2, 3, 4 ] add at 1 place 
+// arr.shift();                   //[ 1, 2, 3, 4 ]    remove from 1 place
+// console.log(arr.at(2));               // 3
+//console.log(arr.indexOf(2));           //1
+//console.log(arr.includes(3));         // true
+// console.log(arr.fill(0));            //[ 0, 0, 0, 0 ]
+// console.log(arr.reverse());          //[ 4, 3, 2, 1 ] 
+// console.log(arr.sort());             //[ 1, 2, 3, 4]
+// console.log(arr.join("-"));          //1-2-3-4
+console.log(arr.toString());            //1,2,3,4
+
+ 
+
+
