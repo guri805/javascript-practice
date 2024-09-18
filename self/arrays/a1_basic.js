dd5b2be8b314d7504455d1ee89c8caf311be185b
Gursangam singh <gursangamsingh2@gmail.com>
2024-09-18 21:02:10 +0530

    modifiaction

--- self/arrays/a1_basic.js
+++ self/arrays/a1_basic.js
@@ -12,17 +12,17 @@ console.log(myArr[0]);
 // console.log(myArr.includes(4));
 // console.log(myArr.indexOf(4));
 
-// const newArr = myArr.join();
+const newArr = myArr.join();
 // console.log(myArr);
 // console.log(newArr); // change to string type
-console.log("orignal" , myArr);
+// console.log("orignal" , myArr);
 
 const newArr1 = myArr.slice(1,3);
-console.log("slice array",newArr1);
-console.log("orignal" , myArr);
+// console.log("slice array",newArr1);
+// console.log("orignal" , myArr);
 const newArr2 = myArr.splice(1,3); // In splice array the orginal array will be changed the splice values will be pop from the orignal array 
-console.log("splice array",newArr2);
-console.log("orignal" , myArr);
+// console.log("splice array",newArr2);
+// console.log("orignal" , myArr);
 
 
 
