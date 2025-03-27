46391225974042f3f8a8ee0702bc17eb69a4dafe
Gursangam singh <gursangamsingh2@gmail.com>
2025-03-27 15:35:19 +0530

    add notes

---
+++ self/javascript_notes/notesFile.js
@@ -0,0 +1,16 @@
+/*
+JAVASCRIPT NOTES 
+
+1.DATATYPES & DATATYPE CONVERSION 
+LINK => https://chatgpt.com/share/67e5010f-e9c8-8000-ad61-b43cd2dc17f1
+
+2. STACK, HEAP MEMORY & CALL BY VALUE, CALL BY REFRENCE 
+LINK => https://chatgpt.com/share/67e51693-f708-8000-9c44-3d6bf19a2427
+
+3. Number And Math Library 
+Link => https://chatgpt.com/share/67e51e09-9aa8-8000-a9ee-df89bf91b9bf
+
+4. Date And Time 
+Link => https://chatgpt.com/share/67e52141-56e8-8000-a8d3-345f58f91c28
+
+*/
