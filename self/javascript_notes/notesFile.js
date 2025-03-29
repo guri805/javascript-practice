923a840bd6b4f38ab2ee615200b47617c95932a6
Gursangam singh <83970607+guri805@users.noreply.github.com>
2025-03-29 15:22:32 +0530

    Update array

--- self/javascript_notes/notesFile.js
+++ self/javascript_notes/notesFile.js
@@ -16,4 +16,7 @@ Link => https://chatgpt.com/share/67e52141-56e8-8000-a8d3-345f58f91c28
 5. String 
 Link => https://chatgpt.com/share/67e7abd9-9848-8000-95d3-2fd35e1807ba
 
+6.Arrays
+Link => https://chatgpt.com/share/67e7c2cc-f2f0-8000-946d-5d28fece7a8e
+
 */
