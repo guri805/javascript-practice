b7da6dc812521f129accefa5e718700b46858361
Gursangam singh <83970607+guri805@users.noreply.github.com>
2025-04-10 14:18:58 +0530

    Update memoization.js

--- self/function/memoization.js
+++ self/function/memoization.js
@@ -51,6 +51,41 @@ function memory(...args) {
 memory(20, 30); // will call calculation
 memory(20, 30); // will return from cache
 
+// with es6 type use closure
+function add(...args){
+  console.log(args) // [ 5, 5, 3, 6, 8, 3 ]
+  let result = args.reduce((acc,curEle) => acc+curEle,0);
+  console.log("result from function",result) // result from function 30
+  return result
+}
+// add(3,5,5,4,3,)
+
+function memoize(fn){
+  const cache= {};
+  console.log(fn) //[Function: add]
+  return function(...args){
+    console.log("memoize args",args) // memoize args [ 5, 5, 3, 6, 8, 3 ]
+    if (args in cache) {
+      console.log("cache result",cache[args]) // cache result 30
+      return cache[args];
+    } else {
+      const result =fn(...args)
+      cache[args]=result
+      console.log("from function",result); // from function 30
+      return result;
+    }
+    
+  }
+}
+const efficient = memoize(add)
+console.time();
+console.log("efficient result",efficient(5,5,3,6,8,3)); // calls function, efficient result 30
+console.timeEnd();
+
+console.time();
+console.log("efficient result",efficient(5,5,3,6,8,3)); // gets from cache, efficient result 30
+console.timeEnd();
+
 // example 2 
 
 // const calculation = (n) => {
