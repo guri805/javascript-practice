e7055637138c4c37fcce3f23a37cba05becb399e
Gursangam singh <83970607+guri805@users.noreply.github.com>
2025-04-10 14:46:59 +0530

    Update memoization.js

--- self/function/memoization.js
+++ self/function/memoization.js
@@ -1,4 +1,5 @@
-// the memoization is an optimization technique that can be used to reduce time-consuming calculations by saving previous input to something called cache and returning the result from it.
+// the memoization is an optimization technique that can be used to reduce time-consuming calculations by saving previous input to
+//something called cache and returning the result from it.
 
 
 // const cache = {};
